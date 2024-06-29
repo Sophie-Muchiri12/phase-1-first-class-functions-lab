20c490ed6cf2ec27a14a83c6cffa5af616abe88f
Sophie Muchiri <muthonisophie12@gmail.com>
2024-06-29 13:10:18 +0300

    advanced firstclass

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(){
+    let ScuberDriver = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    let FirstScuber = ScuberDriver.slice(0,2)
+return FirstScuber
+}
+returnFirstTwoDrivers()
+console.log(returnFirstTwoDrivers())
+
+const returnLastTwoDrivers = function(){
+    let ScuberDriver = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    let LastScuber = ScuberDriver.slice(2,4)
+return LastScuber
+}
+returnLastTwoDrivers()
+console.log(returnLastTwoDrivers())
+
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+
+
+  function createFareMultiplier(multiplier){
+     
+     return function(fare){
+
+        return fare * multiplier
+   }
+
+ }
+    createFareMultiplier(200)
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+
+function selectDifferentDrivers(arrayOfDrivers,funct){
+    
+     return funct(arrayOfDrivers)
+     
+    }
+
+   
